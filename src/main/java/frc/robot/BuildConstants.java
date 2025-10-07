b1cc0c2da00fb5aedde6bf095397905cb3c212a0
Curt <clrozeboom@gmail.com>
2025-10-07 12:38:17 -0400

    Reinstate BlackRobot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC5010Example";
+  public static final String MAVEN_NAME = "TeamBlack2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "7cb77ee59bae63f552afeda16867765ac1d22e2b";
-  public static final String GIT_DATE = "2025-10-05 14:47:51 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-10-07 10:38:30 EDT";
-  public static final long BUILD_UNIX_TIME = 1759847910272L;
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "6b88da14ad57857152383eb368d9ebffd7060202";
+  public static final String GIT_DATE = "2025-10-07 12:30:50 EDT";
+  public static final String GIT_BRANCH = "LobbinLoco";
+  public static final String BUILD_DATE = "2025-10-07 12:36:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1759855010411L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
