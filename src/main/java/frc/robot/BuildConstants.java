e13250ec939b8af150519207471e85fec1da373c
CameronSkea <cameronskea@icloud.com>
2025-10-09 18:31:57 -0400

    for robert
    for robert

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlack2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "34af230763e749d8867c0f36a9b716c3945a1af8";
-  public static final String GIT_DATE = "2025-10-01 17:06:27 EDT";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "b7f64a13ba7f5b114331ca3b57a4c4d99f29faf7";
+  public static final String GIT_DATE = "2025-10-05 15:10:09 EDT";
   public static final String GIT_BRANCH = "7-swerve-updates";
-  public static final String BUILD_DATE = "2025-10-01 17:32:55 EDT";
-  public static final long BUILD_UNIX_TIME = 1759354375311L;
+  public static final String BUILD_DATE = "2025-10-09 18:30:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1760049030617L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
