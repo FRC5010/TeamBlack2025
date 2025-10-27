5689da749d60b41af1dc63bc0d7892e269544731
CameronSkea <cameronskea@icloud.com>
2025-10-27 16:22:21 -0400

    guragura
    jasdf;l;lkfh'

--- src/main/java/frc/robot/blackteam/feeder.java
+++ src/main/java/frc/robot/blackteam/feeder.java
@@ -4,14 +4,7 @@
 
 package frc.robot.blackteam;
 
-import org.frc5010.common.arch.GenericSubsystem;
-
-import com.thethriftybot.ThriftyNova;
-
-
 import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.DegreesPerSecond;
-import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.RPM;
@@ -19,20 +12,10 @@ import static edu.wpi.first.units.Units.Seconds;
 import static yams.mechanisms.SmartMechanism.gearbox;
 import static yams.mechanisms.SmartMechanism.gearing;
 
-import java.util.Map;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
-import org.frc5010.common.arch.GenericSubsystem;
-
 import com.thethriftybot.ThriftyNova;
-
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
+import org.frc5010.common.arch.GenericSubsystem;
 import yams.mechanisms.config.ShooterConfig;
 import yams.mechanisms.velocity.Shooter;
 import yams.motorcontrollers.SmartMotorController;
@@ -41,48 +24,45 @@ import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.local.NovaWrapper;
-import edu.wpi.first.math.controller.ArmFeedforward;
-import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 
 /** Add your docs here. */
 public class feeder extends GenericSubsystem {
-    private final ThriftyNova motor = new ThriftyNova(11);
-      private final SmartMotorControllerConfig motorConfig = new SmartMotorControllerConfig(this)
-      .withGearing(gearing(gearbox(3, 4)))
-      .withIdleMode(MotorMode.BRAKE)
-      .withTelemetry("FeederMotor", TelemetryVerbosity.HIGH)
-      .withStatorCurrentLimit(Amps.of(40))
-      .withMotorInverted(false)
-      .withOpenLoopRampRate(Seconds.of(0.25))
-      .withControlMode(ControlMode.OPEN_LOOP);
+  private final ThriftyNova motor = new ThriftyNova(11);
+  private final SmartMotorControllerConfig motorConfig =
+      new SmartMotorControllerConfig(this)
+          .withGearing(gearing(gearbox(3, 4)))
+          .withIdleMode(MotorMode.BRAKE)
+          .withTelemetry("FeederMotor", TelemetryVerbosity.HIGH)
+          .withStatorCurrentLimit(Amps.of(40))
+          .withMotorInverted(false)
+          .withOpenLoopRampRate(Seconds.of(0.25))
+          .withControlMode(ControlMode.OPEN_LOOP);
 
-      private final SmartMotorController motorController = new NovaWrapper(motor, DCMotor.getNEO(1), motorConfig);
+  private final SmartMotorController motorController =
+      new NovaWrapper(motor, DCMotor.getNEO(1), motorConfig);
 
-      private final ShooterConfig feederConfig = new ShooterConfig(motorController)
-      .withDiameter(Inches.of(4))
-      .withMass(Pounds.of(1))
-      .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH)
-      .withUpperSoftLimit(RPM.of(5000));
+  private final ShooterConfig feederConfig =
+      new ShooterConfig(motorController)
+          .withDiameter(Inches.of(4))
+          .withMass(Pounds.of(1))
+          .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH)
+          .withUpperSoftLimit(RPM.of(5000));
 
-      private final Shooter feeder = new Shooter(feederConfig);
-      
-      public Command setSpeed(double speed) {
-        return feeder.set(speed);
-      }
-    public feeder(){
-        
-       
-   }
+  private final Shooter feeder = new Shooter(feederConfig);
 
-   @Override
-   public void periodic() {
-     feeder.updateTelemetry();
-   }
-   @Override
-   public void simulationPeriodic() {
-     feeder.simIterate();
-   }
+  public Command setSpeed(double speed) {
+    return feeder.set(speed);
+  }
+
+  public feeder() {}
+
+  @Override
+  public void periodic() {
+    feeder.updateTelemetry();
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    feeder.simIterate();
+  }
 }
