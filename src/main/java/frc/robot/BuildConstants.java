ac5588d326d5093eabc7c5c0ff4f6b63d4e6e077
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2025-11-15 12:15:51 -0500

    Working Black YAGSL values

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "TeamBlack2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "f3b5347a52a11968dc93cd8e71d2b8ec31a43d7e";
-  public static final String GIT_DATE = "2025-11-12 18:29:47 EST";
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "a173f36d342264a3151b276faeabf4662d59fcea";
+  public static final String GIT_DATE = "2025-11-12 18:56:43 EST";
   public static final String GIT_BRANCH = "LobbinLoco";
-  public static final String BUILD_DATE = "2025-11-12 18:40:13 EST";
-  public static final long BUILD_UNIX_TIME = 1762990813639L;
+  public static final String BUILD_DATE = "2025-11-15 11:38:54 EST";
+  public static final long BUILD_UNIX_TIME = 1763224734062L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
