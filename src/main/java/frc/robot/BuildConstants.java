e53b495a1f87727e17e2ed1261a67b148dbb8b8a
Curt <clrozeboom@gmail.com>
2025-11-27 10:04:45 -0500

    Add Javadoc generation and update configs
    Introduces a GitHub Actions workflow for Javadoc generation and adds a Javadoc configuration block to build.gradle. Updates simgui.json to adjust device and window settings, and refreshes build metadata in BuildConstants.java.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC5010Example";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "d1fab7199f049143530e3949b0233e1b795bd4e1";
-  public static final String GIT_DATE = "2025-11-15 16:30:56 EST";
+  public static final int GIT_REVISION = 41;
+  public static final String GIT_SHA = "9d920260053ef3b21a30dc20887778087d03bf74";
+  public static final String GIT_DATE = "2025-11-23 16:11:21 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-11-16 22:20:42 EST";
-  public static final long BUILD_UNIX_TIME = 1763349642870L;
+  public static final String BUILD_DATE = "2025-11-23 17:36:47 EST";
+  public static final long BUILD_UNIX_TIME = 1763937407349L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
